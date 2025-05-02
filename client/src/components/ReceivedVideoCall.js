ab0850968ae0ad38c1ee313fcc427fb3f3ac640a
Akadeha420 <aungkhamnaung2003@gmail.com>
2025-05-02 14:57:09 -0500

    implement displaying videos in ReceivedVideoCall.js

--- client/src/components/ReceivedVideoCall.js
+++ client/src/components/ReceivedVideoCall.js
@@ -1,8 +1,12 @@
-import {useCallback, useEffect, useRef} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import Peer from "simple-peer";
 
 function ReceivedVideoCall({mySocketId, myStream, othersSocketId, webrtcSocket, offer}) {
   const peerRef = useRef(null);
+  const [othersVideo, setOthersVideo] = useState(null); // State to store others video
+  const setVideoNode = useCallback(videoNode => { // Displaying Video Stream
+    videoNode && (videoNode.srcObject = othersVideo);
+  }, [othersVideo]);
   
   const createPeer = useCallback((othersSocketId, mySocketId, myStream, webrtcSocket, offer) => {
     const peer = new Peer({
@@ -26,9 +30,36 @@ function ReceivedVideoCall({mySocketId, myStream, othersSocketId, webrtcSocket, 
   
   useEffect(() => {
     peerRef.current = createPeer(othersSocketId, mySocketId, myStream, webrtcSocket, offer);
+    
+    // Event handler to set video
+    const handleSentSignal = (signal) => {
+      if(signal.fromUser === othersSocketId && peerRef.current) {
+        console.log("Received returned signal", signal.fromUser);
+        peerRef.current.signal(signal.offer)
+      }
+    }
+    
+    const handleStream = (stream) => {
+      console.log("Received stream from peer");
+      setOthersVideo(stream);
+    }
+    
+    webrtcSocket.on('forwardSignal', handleSentSignal);
+    peerRef.current.on('stream', handleStream);
+    
+    return () => {
+      webrtcSocket.off("forwardSignal", handleSentSignal);
+      
+      if (peerRef.current) {
+        peerRef.current.off("stream", handleStream);
+        peerRef.current.destroy();
+      }
+    }
   }, [mySocketId, othersSocketId, myStream, webrtcSocket]);
   
-  return <></>;
+  return <>
+    {othersVideo && <video width="200px" ref={setVideoNode} autoPlay={true}/>}
+  </>;
 }
 
 export default ReceivedVideoCall;
